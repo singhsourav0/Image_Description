a9daaf2615f1bbafc3df1224a97077d8124fa1f3
Sourav Kumar <95288159+singhsourav0@users.noreply.github.com>
2023-12-31 13:45:02 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, redirect, url_for
+from PIL import Image
+from io import BytesIO
+
+from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+import torch
+
+app = Flask(__name__)
+
+# Load model and tokenizer
+model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+def predict_step(image_path):
+    i_image = Image.open(image_path)
+    if i_image.mode != "RGB":
+        i_image = i_image.convert(mode="RGB")
+
+    pixel_values = feature_extractor(images=[i_image], return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+
+    output_ids = model.generate(pixel_values, **gen_kwargs)
+
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+    return preds[0]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return redirect(url_for('index'))
+
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(url_for('index'))
+
+    # Process the uploaded image
+    uploaded_caption = predict_step(file)
+
+    return render_template('result.html', caption=uploaded_caption)
+
+if __name__ == '__main__':
+    app.run(debug=True)
